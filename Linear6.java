02b162a5beca39f5748e921e14f85c97477e5000
it-maks <85013643+it-maks@users.noreply.github.com>
2021-07-06 19:13:36 +0300

    Add files via upload

---
+++ Linear6.java
@@ -0,0 +1,71 @@
+package by.jonline.module01.tasks26_30;
+
+import java.util.Scanner;
+
+// Для данной области составить линейную программу, которая печатает true, 
+// если точка с координатами (x, y) принадлежит закрашенной области и 
+// false в противном случае
+
+
+public class Linear6 {
+
+	public static void main(String[] args) {
+		
+		int x = enterN("Введите x >> ");
+		int y = enterN("Введите y >> ");	
+		
+		checkB(x, y);
+		checkC(x, y);
+
+	}
+	
+	public static int enterN(String s) {
+
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+
+		int t;
+
+		System.out.println(s);
+		while (!sc.hasNextInt()) {
+			sc.next();
+			System.out.println("Введите еще раз >> ");
+		}
+		t = sc.nextInt();
+
+		return t;
+	}
+	
+
+	
+	// метод checkB проверяет принадлежит ли точка области b) в задаче
+	
+	public static void checkB(int a, int b) {
+		
+		boolean check = false;
+		
+		if (b > 0 & b <= 4 & a >= -2 & a <= 2) {
+			check = true;
+		}
+		if (b <= 0 & b >= -3 & a >= -4 & a < 4) {
+			check = true;
+		}	
+		System.out.println(check);
+	}
+	
+	// метод checkC проверяет принадлежит ли точка области b) в задаче
+	
+	public static void checkC(int a, int b) {
+		
+		boolean check = false;
+		
+		if (a > 0 & a <= 5 & b <= 0 & b >= -5 & (a * a + b * b) <= 5 * 5) {
+			check = true;
+		}
+		if (a > 0 & a <= 4 & b > 0 & b <= 4 & (a * a + b * b) <= 4 * 4) {
+			check = true;
+		}	
+		System.out.println(check);
+	}
+
+}
